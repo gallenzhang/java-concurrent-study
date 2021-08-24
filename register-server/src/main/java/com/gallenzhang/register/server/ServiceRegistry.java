147bfebc02554a5c4122cdd3b28cf2155ac22aeb
gallenzhang <zhangxiaoqiang0722@gmail.com>
2021-08-24 21:22:49 +0800

    服务注册中心的最近更新服务实例队列实现

--- register-server/src/main/java/com/gallenzhang/register/server/ServiceRegistry.java
+++ register-server/src/main/java/com/gallenzhang/register/server/ServiceRegistry.java
@@ -1,6 +1,7 @@
 package com.gallenzhang.register.server;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 /**
@@ -11,13 +12,28 @@ import java.util.Map;
  */
 public class ServiceRegistry {
 
+    public static final Long RECENTLY_CHANGED_ITEM_CHECK_INTERVAL = 3000L;
+    public static final Long RECENTLY_CHANGED_ITEM_EXPIRED = 3 * 60 * 1000L;
+
+
     /**
      * 注册表是一个单例
      */
     private static ServiceRegistry instance = new ServiceRegistry();
 
-    private ServiceRegistry() {
+    /**
+     * 最近变更的服务实例的队列
+     */
+    private LinkedList<RecentlyChangedServiceInstance> recentlyChangedQueue = new LinkedList<>();
 
+    /**
+     * 构造函数
+     */
+    private ServiceRegistry() {
+        //启动后台线程监控最近变更的队列
+        RecentlyChangedQueueMonitor recentlyChangedQueueMonitor = new RecentlyChangedQueueMonitor();
+        recentlyChangedQueueMonitor.setDaemon(true);
+        recentlyChangedQueueMonitor.start();
     }
 
     /**
@@ -36,12 +52,19 @@ public class ServiceRegistry {
      * @param serviceInstance
      */
     public synchronized void register(ServiceInstance serviceInstance) {
+        //将服务实例放入最近变更的队列中
+        RecentlyChangedServiceInstance recentlyChangedServiceInstance = new RecentlyChangedServiceInstance(
+                serviceInstance,
+                System.currentTimeMillis(),
+                ServiceInstanceOperation.REGISTER);
+        recentlyChangedQueue.offer(recentlyChangedServiceInstance);
+
+        //将服务实例放入注册表中
         Map<String, ServiceInstance> serviceInstanceMap = registry.get(serviceInstance.getServiceName());
         if (serviceInstanceMap == null) {
-            serviceInstanceMap = new HashMap<String, ServiceInstance>();
+            serviceInstanceMap = new HashMap<>();
             registry.put(serviceInstance.getServiceName(), serviceInstanceMap);
         }
-
         serviceInstanceMap.put(serviceInstance.getServiceInstanceId(), serviceInstance);
 
         System.out.println("服务实例完成注册......[" + serviceInstance + "]");
@@ -77,7 +100,19 @@ public class ServiceRegistry {
      */
     public synchronized void remove(String serviceName, String serviceInstanceId) {
         System.out.println("服务实例从注册表中摘除 [" + serviceName + ", " + serviceInstanceId + "] ");
+
+        //从服务注册表删除服务实例
         Map<String, ServiceInstance> serviceInstanceMap = registry.get(serviceName);
+        ServiceInstance serviceInstance = serviceInstanceMap.get(serviceInstanceId);
+
+        //将服务实例放入最近变更的队列中
+        RecentlyChangedServiceInstance recentlyChangedServiceInstance = new RecentlyChangedServiceInstance(
+                serviceInstance,
+                System.currentTimeMillis(),
+                ServiceInstanceOperation.REMOVE);
+        recentlyChangedQueue.offer(recentlyChangedServiceInstance);
+
+        //从服务注册表删除服务实例
         serviceInstanceMap.remove(serviceInstanceId);
     }
 
@@ -88,5 +123,73 @@ public class ServiceRegistry {
      */
     public static ServiceRegistry getInstance() {
         return instance;
+    }
+
+    /**
+     * 最近变化的服务实例
+     */
+    class RecentlyChangedServiceInstance {
+        /**
+         * 服务实例
+         */
+        ServiceInstance serviceInstance;
+
+        /**
+         * 发生变更的时间戳
+         */
+        Long changedTimestamp;
+
+        /**
+         * 变更操作
+         */
+        String serviceInstanceOperation;
+
+        public RecentlyChangedServiceInstance(ServiceInstance serviceInstance, Long changedTimestamp, String serviceInstanceOperation) {
+            this.serviceInstance = serviceInstance;
+            this.changedTimestamp = changedTimestamp;
+            this.serviceInstanceOperation = serviceInstanceOperation;
+        }
+    }
+
+    /**
+     * 服务实例操作
+     */
+    class ServiceInstanceOperation {
+        /**
+         * 注册
+         */
+        public static final String REGISTER = "register";
+
+        /**
+         * 删除
+         */
+        public static final String REMOVE = "remove";
+    }
+
+    /**
+     * 最近变更队列的监控线程
+     */
+    class RecentlyChangedQueueMonitor extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    synchronized (instance) {
+                        RecentlyChangedServiceInstance recentlyChangedServiceInstance;
+                        Long currentTimestamp = System.currentTimeMillis();
+                        while ((recentlyChangedServiceInstance = recentlyChangedQueue.peek()) != null) {
+                            if (currentTimestamp - recentlyChangedServiceInstance.changedTimestamp >
+                                    RECENTLY_CHANGED_ITEM_EXPIRED) {
+                                recentlyChangedQueue.pop();
+                            }
+                        }
+                    }
+
+                    Thread.sleep(RECENTLY_CHANGED_ITEM_CHECK_INTERVAL);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
