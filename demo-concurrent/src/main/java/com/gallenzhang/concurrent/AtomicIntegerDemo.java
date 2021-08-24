7b4685f75e295fe1b50d74f3a609cc32d2df3553
xiaoqiang.zhang <xiaoqiang.zhang@shopee.com>
2021-08-24 21:27:46 +0800

    AtomicInteger使用demo示例

--- demo-concurrent/src/main/java/com/gallenzhang/concurrent/AtomicIntegerDemo.java
+++ demo-concurrent/src/main/java/com/gallenzhang/concurrent/AtomicIntegerDemo.java
@@ -15,15 +15,31 @@ public class AtomicIntegerDemo {
     static AtomicInteger j = new AtomicInteger(0);
 
     public static void main(String[] args) {
-        synchronizedAdd();
+        //synchronizedAdd();
+        atomicAdd();
     }
 
     private static void synchronizedAdd() {
         for (int i = 0; i < 10; i++) {
-            new Thread(){
+            new Thread() {
                 @Override
                 public void run() {
-                    System.out.println(++AtomicIntegerDemo.i);
+                    //10个线程就要依次的慢慢的一个一个的进入锁代码块，然后依次对i变量进行i++操作
+                    //每次操作完i++，就写回主存，下一个线程从主存来加载，再次i++，并发性降低了
+                    synchronized (AtomicIntegerDemo.class) {
+                        System.out.println(++AtomicIntegerDemo.i);
+                    }
+                }
+            }.start();
+        }
+    }
+
+    private static void atomicAdd() {
+        for (int i = 0; i < 10; i++) {
+            new Thread() {
+                @Override
+                public void run() {
+                    System.out.println(AtomicIntegerDemo.j.incrementAndGet());
                 }
             }.start();
         }
